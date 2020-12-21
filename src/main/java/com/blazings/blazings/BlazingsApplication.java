d92625dc753236774fd580b4a5cccd320f34ade5
JBlazingSun <blazings@vip.qq.com>
2020-12-21 15:34:15 +0800

    init

---
+++ src/main/java/com/blazings/blazings/BlazingsApplication.java
@@ -0,0 +1,13 @@
+package com.blazings.blazings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlazingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlazingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blazings/blazings/BlazingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blazings.blazings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlazingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
