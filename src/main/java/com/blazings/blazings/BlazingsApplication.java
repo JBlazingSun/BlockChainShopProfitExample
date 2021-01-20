ee4c5874f9b22dae5345282cd478097a2faa13d6
JBlazingSun <blazings@vip.qq.com>
2021-01-20 21:15:29 +0800

    add

--- src/main/java/com/blazings/blazings/BlazingsApplication.java
+++ src/main/java/com/blazings/blazings/BlazingsApplication.java
@@ -19,34 +19,52 @@ public class BlazingsApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Profit profitTotal = new Profit();
-        int totalDay = 10;
+        Profit profitToday = new Profit();
+        int totalDay = 7;
         int fewPeople = 5;
+        Double toDayInvertMoney = 1000.0;
         List<People> peopleList = new ArrayList<>();
         //累计发行
         Double totalScore = 0.0;
         //每天最大发行
         Double todayMaxScore = 50000000.0/365;
-        //今天发行量
-        Double todayScore = 0.0;
-
+        profitToday.setMember(todayMaxScore*0.5);
+        profitToday.setNode(todayMaxScore*0.14);
+        profitToday.setPlatform(todayMaxScore*0.24);
+        profitToday.setProvince(todayMaxScore*0.02);
+        profitToday.setRegion(todayMaxScore*0.01);
+        profitToday.setTech(todayMaxScore*0.06);
         for (int i = 0; i < totalDay; i++) {
+            totalScore+=todayMaxScore;
             for (int j = 0; j < fewPeople; j++) {
                 People people = new People();
                 people.setJoinDay(i);
                 people.setName(String.valueOf(i));
-                people.setTodayInvestMoney(1000.0);
-//                people.setTotalInvestMoney(people.getTodayInvestMoney()+1000.0);
-
-//                members.add(new Member());
+                people.setTodayScore(profitToday.getMember()/fewPeople+
+                        profitToday.getNode()/fewPeople);
+                people.setTotalScore(people.getTodayScore());
+                people.setTodayInvestMoney(toDayInvertMoney);
+                people.setTotalInvestMoney(0.0D);
+                people.setTotalValueMoney(0.0D);
+            }
+            if (profitTotal.getMember()!=null){
+                profitTotal.setMember(profitTotal.getMember()+profitToday.getMember());
+                profitTotal.setNode(profitTotal.getNode()+profitToday.getNode());
+                profitTotal.setPlatform(profitTotal.getPlatform()+profitToday.getPlatform());
+                profitTotal.setProvince(profitTotal.getProvince()+profitToday.getProvince());
+                profitTotal.setRegion(profitTotal.getRegion()+profitToday.getRegion());
+                profitTotal.setTech(profitTotal.getTech()+profitToday.getTech());
+            }
+            //firstAdd
+            else {
+                profitTotal.setMember(profitToday.getMember());
+                profitTotal.setNode(profitToday.getNode());
+                profitTotal.setPlatform(profitToday.getPlatform());
+                profitTotal.setProvince(profitToday.getProvince());
+                profitTotal.setRegion(profitToday.getRegion());
+                profitTotal.setTech(profitToday.getTech());
             }
         }
-
-        profitTotal.setMember(todayScore*0.5);
-        profitTotal.setNode(todayScore*0.14);
-        profitTotal.setPlatform(todayScore*0.24);
-        profitTotal.setProvince(todayScore*0.02);
-        profitTotal.setRegion(todayScore*0.01);
-        profitTotal.setTech(todayScore*0.06);
 
         System.out.println(todayMaxScore);
     }
